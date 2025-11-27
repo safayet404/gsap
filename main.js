622eb6237013f762438a73badd8b0aafc4cc4d4b
Safayet <hossainsafayet187@gmail.com>
2025-11-28 01:54:04 +0600

    pin

--- main.js
+++ main.js
@@ -1,60 +1,12 @@
-// gsap.to(".box1", {
-//   x: 1500,
-//   rotate: 360,
-//   duration: 1.5,
-//   delay: 1,
-//   repeat: -1,
-//   yoyo: true,
-// });
-
-// gsap.to(".box2", {
-//   x: 1500,
-//   rotate: 360,
-//   backgroundColor: "yellow",
-//   duration: 1.5,
-//   delay: 2.5,
-// });
-
-// tl.to(".box1", {
-//   x: 1500,
-//   rotate: 360,
-//   duration: 2,
-//   delay: 1,
-// });
-
-// tl.to(".box2", {
-//   x: 1500,
-//   duration: 2,
-//   delay: 1.5,
-// });
-
-// tl.to(".box3", {
-//   x: 1500,
-//   duration: 2,
-//   rotate: -360,
-//   delay: 1,
-// });
-
-var tl = gsap.timeline();
-
-tl.from("h2", {
-  y: -30,
-  opacity: 0,
-  delay: 1,
-  duration: 0.5,
-});
-
-tl.from("h4", {
-  y: -30,
-  opacity: 0,
-  delay: 1,
-  duration: 0.5,
-  stagger: 0.5,
-});
-
-tl.from("h1", {
-  y: 20,
-  opacity: 0,
-  duration: 0.5,
-  scale: 0.2,
+gsap.to("#page2 h1", {
+  xPercent: -150, // better than using transform string
+  scrollTrigger: {
+    trigger: "#page2 h1",
+    scroller: "body",
+    markers: true,
+    start: "top 80%", // when h1 is a bit lower in viewport
+    end: "top 20%",
+    scrub: true, // tie animation progress to scroll
+    pin: true,
+  },
 });
